6e65ffaf7aeb4e76da0b00ad5ad200ecdeb930c2
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-04-12 01:40:37 -0400

    Fixed filter for deleting friends

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,4 +1,4 @@
-import { ADD_TODO, TOGGLE_TODO } from '../actions';
+import { ADD_TODO, TOGGLE_TODO, DELETE_TODO} from '../actions';
 
 
 const initialState = {
@@ -38,6 +38,13 @@ export default (state = initialState, action) => {
                     } else {
                         return todo;
                     }
+                })
+            }
+        case DELETE_TODO:
+            return {
+                ...state,
+                todos: state.todos.filter(todo => {
+                    return !todo.completed
                 })
             }
         default:
